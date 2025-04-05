907aae73cc5b28d4a7fbbd35317a4223c2651b1b
Splasz <splasz303@gmail.com>
2025-04-05 21:05:23 +0200

    fix(DashboardStats): handle backend response as object instead of array

--- src/features/dashboard/DashboardStats.tsx
+++ src/features/dashboard/DashboardStats.tsx
@@ -5,15 +5,13 @@ import { FaCarAlt } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import React from "react";
 
-type Stats = {
-  income: number;
-  tasks: number;
-  new_clients: number;
-  repair_cars: number;
-};
-
 function DashboardStats() {
-  const [statistics, setStatistics] = useState<Stats[]>([]);
+  const [statistics, setStatistics] = useState<{
+    income: number;
+    tasks: number;
+    newClients: number;
+    repairNum: number;
+  } | null>(null);
 
   useEffect(() => {
     fetch("http://localhost/react_backend/statistics.php")
@@ -24,59 +22,57 @@ function DashboardStats() {
 
   return (
     <div className="flex w-full">
-      {statistics.map((z, idx) => (
-        <React.Fragment key={idx}>
-          <div className="flex flex-col p-2.5 w-full border-r border-r-gray-300">
-            <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
-              <div>
-                <SiBitcoincash className="size-10 text-accent2" />
-              </div>
-              <div className="opacity-50">Przychód</div>
+      <React.Fragment>
+        <div className="flex flex-col p-2.5 w-full border-r border-r-gray-300">
+          <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
+            <div>
+              <SiBitcoincash className="size-10 text-accent2" />
             </div>
-            <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
-              <div className="opacity-50">zł</div>
-              <div>{z.income}</div>
-            </div>
+            <div className="opacity-50">Przychód</div>
           </div>
+          <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
+            <div className="opacity-50">zł</div>
+            <div>{statistics?.income}</div>
+          </div>
+        </div>
 
-          <div className="flex flex-col p-2.5 w-full border-r border-r-gray-300">
-            <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
-              <div>
-                <MdOutlineTaskAlt className="size-10 text-accent" />
-              </div>
-              <div className="opacity-50">Zlecenia</div>
+        <div className="flex flex-col p-2.5 w-full border-r border-r-gray-300">
+          <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
+            <div>
+              <MdOutlineTaskAlt className="size-10 text-accent" />
             </div>
-            <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
-              <div>{z.tasks}</div>
-            </div>
+            <div className="opacity-50">Zlecenia</div>
           </div>
+          <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
+            <div>{statistics?.tasks}</div>
+          </div>
+        </div>
 
-          <div className="flex flex-col p-2.5 w-full border-r border-r-gray-300">
-            <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
-              <div>
-                <FiUserPlus className="size-10 text-secondary" />
-              </div>
-              <div className="opacity-50">Nowi Klienci</div>
+        <div className="flex flex-col p-2.5 w-full border-r border-r-gray-300">
+          <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
+            <div>
+              <FiUserPlus className="size-10 text-secondary" />
             </div>
-            <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
-              <div className="opacity-50">+</div>
-              <div>{z.new_clients}</div>
-            </div>
+            <div className="opacity-50">Nowi Klienci</div>
           </div>
+          <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
+            <div className="opacity-50">+</div>
+            <div>{statistics?.newClients}</div>
+          </div>
+        </div>
 
-          <div className="flex flex-col p-2.5 w-full">
-            <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
-              <div>
-                <FaCarAlt className="size-10 text-primary" />
-              </div>
-              <div className="opacity-50">Naprawione Auta</div>
+        <div className="flex flex-col p-2.5 w-full">
+          <div className="flex items-center text-2xl font-medium gap-6 p-2.5 pl-3.5">
+            <div>
+              <FaCarAlt className="size-10 text-primary" />
             </div>
-            <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
-              <div>{z.repair_cars}</div>
-            </div>
+            <div className="opacity-50">Naprawione Auta</div>
           </div>
-        </React.Fragment>
-      ))}
+          <div className="flex items-center justify-center text-5xl p-2.5 gap-2.5">
+            <div>{statistics?.repairNum}</div>
+          </div>
+        </div>
+      </React.Fragment>
     </div>
   );
 }
